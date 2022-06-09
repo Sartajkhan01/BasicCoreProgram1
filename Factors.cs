e6d4ee9a6d89f874fd75823ada8a1ff89e843dfc
Sartajkhan01 <106169841+Sartajkhan01@users.noreply.github.com>
2022-06-09 23:14:30 +0530

    Add files via upload

---
+++ Factors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class Factors
+    {
+        public void Factor()
+        {
+            Console.WriteLine("Enter the number: ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Prime Factors for [0] are, ", Number);
+
+            for (int i = 2; 1 * i <= Number; i++)
+            {
+                if (Number % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
