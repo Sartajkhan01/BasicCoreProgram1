f3d64ddd672a240bbbb37b1c5676865d5bdf3a99
Sartajkhan01 <106169841+Sartajkhan01@users.noreply.github.com>
2022-06-09 23:20:38 +0530

    Add files via upload

---
+++ PercentageOfHeadAndTails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class PercentageOfHeadAndTails
+    {
+        private static int tails;
+
+        public static void main(String[] args)
+        {
+            int loopCount = 1;
+            int numFlips = 10;
+            double result;
+            int heads = 0;
+
+            while (loopCount < numFlips)
+            {
+                result = Math.random();
+                if (result <= .5) { heads++; }
+                loopCount++;
+            }
+
+            double percentHeads = heads / numFlips * 100;
+            double percentTails = (numFlips - tails) / numFlips * 100;
+        }
+    }
+}
+
+
