4e8588468d5122fdcbd5dc5f95e03c468310385e
Sartajkhan01 <106169841+Sartajkhan01@users.noreply.github.com>
2022-06-09 23:18:05 +0530

    Add files via upload

---
+++ LeapYear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Year in Four Digits: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine("{0} is a Leap Year", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year", year);
+            }
+            Console.ReadKey();
+        }
+    }
+}
