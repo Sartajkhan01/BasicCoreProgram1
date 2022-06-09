01e2c0b054634aa48c62031f45b0af53fdb7ad42
Sartajkhan01 <106169841+Sartajkhan01@users.noreply.github.com>
2022-06-09 23:21:06 +0530

    Add files via upload

---
+++ Power2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class Power2
+    {
+        public void Power_of_Two()
+        {
+            Console.Write("Please enter a number: ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            while (Number < 0 || Number >= 31 )
+            {
+                Console.Write("Please enter a valid number: ");
+                Number = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i <= Number; i++)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+        }
+    }
+}
